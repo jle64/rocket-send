a07883f554f579b31c4851c746399e8b3a9806d4
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2017-03-06 03:12:42 +0100

    Add astrology pictures modules.

---
+++ rocket/modules/astropix.py
@@ -0,0 +1,16 @@
+"Gets astronomy picture of the day from NASA."
+import requests as r
+from bs4 import BeautifulSoup
+
+def get_message():
+    site_url = 'https://apod.nasa.gov/apod/'
+    page_url = site_url + 'astropix.html'
+    page = r.get(page_url).text
+    soup = BeautifulSoup(page, "html.parser")
+    # text between the first <b></b> tags should be the title
+    img_title = soup.find_all('b')[0].contents[0]
+    # src attribute of first <img> tag should be a relative path
+    img_path = soup.find_all('img')[0].get('src')
+    img_url = site_url + img_path
+
+    return {'title': img_title, 'text': img_url}
