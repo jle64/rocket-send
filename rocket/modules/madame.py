8875f2f8b532e82db1ea9e13b28fc3da22f538f8
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2017-11-16 18:53:28 +0100

    Use tumblr instead of rss for madame module

--- rocket/modules/madame.py
+++ rocket/modules/madame.py
@@ -1,17 +1,15 @@
 "Gets last picture from Bonjour Madame."
-import feedparser
+import requests as r
 from bs4 import BeautifulSoup
 from rocket.modulebase import ModuleBase
 
 
 class Module(ModuleBase):
     def __get_message__(self, args=None):
-        feed_url = 'http://feeds.feedburner.com/BonjourMadame?format=xml'
-        rss = feedparser.parse(feed_url)
-        last_post = rss['entries'][0]
-        img_title = last_post['title']
-        summary = last_post['summary_detail']['value']
-        soup = BeautifulSoup(summary, "html.parser")
-        img_url = soup.find_all('img')[0].get('src')
-
+        page_url = 'http://dites.bonjourmadame.fr/'
+        page = r.get(page_url).text
+        soup = BeautifulSoup(page, "html.parser")
+        img = soup.find_all('img')[3]
+        img_url = img.get('src')
+        img_title = img.get('alt')
         return {'title': img_title, 'text': img_url}
